a8b6a78e71f09d4fbcf369014d40faf6148a21b3
Anuj Kumar <personeluse2000@gmail.com>
2026-04-01 18:29:44 +0530

    java

--- Solution.java
+++ Solution.java
@@ -1,52 +1,16 @@
-import java.util.*;
-
 class Solution {
-
-    int n;
-    List<List<Integer>> result = new ArrayList<>();
-
-    void backtrack(List<Integer> temp, HashMap<Integer, Integer> mp){
-
-        if(temp.size() == n){
-            result.add(new ArrayList<>(temp));
-            return;
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReachable = 0;
+        int n            = nums.size();
+        
+        for(int i = 0; i<n; i++) {
+            if(i > maxReachable) {
+                return false;
+            }
+            maxReachable = max(maxReachable, nums[i]+i);
         }
-
-        for(Integer num : mp.keySet()){
-
-            int count = mp.get(num);
-
-            if(count == 0)
-                continue;
-
-            // do something
-            temp.add(num);
-            mp.put(num, count - 1);
-
-            // explore
-            backtrack(temp, mp);
-
-            // undo
-            temp.remove(temp.size()-1);
-            mp.put(num, count);
-        }
+        
+        return true;
     }
-
-    public List<List<Integer>> permuteUnique(int[] nums) {
-
-        n = nums.length;
-
-        HashMap<Integer,Integer> mp = new HashMap<>();
-
-        // count frequency
-        for(int num : nums){
-            mp.put(num, mp.getOrDefault(num,0) + 1);
-        }
-
-        List<Integer> temp = new ArrayList<>();
-
-        backtrack(temp, mp);
-
-        return result;
-    }
-}
+};
