853f4269ed57b2e85843f350a1967a906696d72f
Anuj Kumar <personeluse2000@gmail.com>
2026-04-06 18:08:59 +0530

    code

--- Solution.java
+++ Solution.java
@@ -1,37 +1,41 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] g) {
-        int m = g.length;
-        int n = g[0].length;
-        int[][] t = new int[m][n];
 
-        // Fix 1: Correct start cell initialization
-        t[0][0] = (g[0][0] == 1) ? 0 : 1;
+    int m, n;
 
-        // Fix 2: First row (simplified + corrected)
-        for (int col = 1; col < n; col++) {
-            if (g[0][col] == 1)
-                t[0][col] = 0;
-            else
-                t[0][col] = t[0][col - 1];
-        }
-        // Fix 3: First column (corrected indices)
-        for (int row = 1; row < m; row++) {
-            if (g[row][0] == 1)
-                t[row][0] = 0; 
-            else
-                t[row][0] = t[row - 1][0];
-        }
- 
-        // Fix 4: Declare i properly
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (g[i][j] == 1)
-                    t[i][j] = 0;
-                else
-                    t[i][j] = t[i - 1][j] + t[i][j - 1];
+    public boolean exist(char[][] g, String s) {
+
+        m = g.length;
+        n = g[0].length;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(g[i][j] == s.charAt(0) && solve(s, g, i, j, 0)){
+                    return true;
+                }
             }
         }
+        return false;
+    }
 
-        return t[m - 1][n - 1];
+    boolean solve(String s, char[][] g, int i, int j, int k){
+
+        if(k == s.length())
+            return true;
+
+        if(i < 0 || j < 0 || i >= m || j >= n || g[i][j] != s.charAt(k))
+            return false;
+
+        char temp = g[i][j];
+        g[i][j] = '0';   // mark visited
+
+        boolean found =
+                solve(s, g, i-1, j, k+1) ||
+                solve(s, g, i+1, j, k+1) ||
+                solve(s, g, i, j-1, k+1) ||
+                solve(s, g, i, j+1, k+1);
+
+        g[i][j] = temp;  // backtrack
+
+        return found;
     }
 }
