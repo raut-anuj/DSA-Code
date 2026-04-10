fa77a9f7689487e51055b1b2b5103e5c7d16d87b
Anuj Kumar <personeluse2000@gmail.com>
2026-04-10 19:05:58 +0530

    code

--- Solution.java
+++ Solution.java
@@ -1,34 +1,29 @@
-class Solution {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        int candy = n; //Each person given one candy
-
-        int i = 1;
-        while (i < n) {
-
-            if (ratings[i] == ratings[i - 1]) {
-                i++;
-                continue;
-            }
-
-            //For increasing slope
-            int peak = 0;
-            while (i < n && ratings[i] > ratings[i - 1]) {
-                peak++;
-                candy += peak;
-                i++;
-            }
-
-            //For decreasing slope
-            int valley = 0;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                valley++;
-                candy += valley; 
-                i++;   
-            }
-            candy -= Math.min(peak, valley);
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
         }
 
-        return candy;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast != slow) {
+            return null;
+        }
+
+        ListNode entry = head;
+        while (entry != slow) {
+            entry = entry.next;
+            slow = slow.next;
+        }
+        return entry;
     }
 }
