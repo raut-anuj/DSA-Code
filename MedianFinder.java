1583b7ed00468e69ba5cc10f34d3665ab26c9728
Anuj Kumar <personeluse2000@gmail.com>
2026-04-13 21:49:16 +0530

    change

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,35 +1,5 @@
-public class MedianFinder {
-    private PriorityQueue<Integer> leftMaxHeap; // max heap
-    private PriorityQueue<Integer> rightMinHeap; // min heap
-
-    public MedianFinder() {
-        leftMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        rightMinHeap = new PriorityQueue<>();
-    }
-
-    public void addNum(int num) {
-        if (leftMaxHeap.isEmpty() || num < leftMaxHeap.peek()) {
-            leftMaxHeap.add(num);
-        } else {
-            rightMinHeap.add(num);
-        }
-
-        // always maintain leftMaxHeap size one greater than rightMinHeap size
-        // or both sizes equal
-        if (Math.abs(leftMaxHeap.size() - rightMinHeap.size()) > 1) {
-            rightMinHeap.add(leftMaxHeap.poll());
-        } else if (leftMaxHeap.size() < rightMinHeap.size()) {
-            leftMaxHeap.add(rightMinHeap.poll());
-        }
-    }
-
-    public double findMedian() {
-        if (leftMaxHeap.size() == rightMinHeap.size()) {
-            // even number of elements
-            return (double) (leftMaxHeap.peek() + rightMinHeap.peek()) / 2;
-        }
-
-        // odd number of elements
-        return leftMaxHeap.peek();
+class Solution {
+    public int change(int amount, int[] coins) {
+        
     }
 }
