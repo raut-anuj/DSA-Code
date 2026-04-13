8006d5ca6f1a02a3f7b5cc87d4fd67765c1aa5fd
Anuj Kumar <personeluse2000@gmail.com>
2026-04-13 21:56:57 +0530

    code

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,5 +1,17 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        
+
+    public int change(int t, int[] c) {
+        int t[]
+        return solve(0, 0, t, c);
+    }
+
+    public int solve(int s, int i, int t, int[] arr) {
+        if (s == t) return 1;
+        if (s > t || i >= arr.length) return 0;
+
+        int take = solve(s + arr[i], i, t, arr);
+        int skip = solve(s, i + 1, t, arr);
+
+        return take + skip;
     }
 }
