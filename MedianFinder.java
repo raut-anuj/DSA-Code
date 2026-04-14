d63bf1e60303f6e665f764e5a2bfea4391e88679
Anuj Kumar <personeluse2000@gmail.com>
2026-04-14 22:47:20 +0530

    code 121

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,17 +1,13 @@
 class Solution {
-    
-    public int change(int t, int[] c) {
-        int t[];    
-        return solve(0, 0, t, c);
-    }
-  
-    public int solve(int s, int i, int t, int[] arr) {
-        if (s == t) return 1;
-        if (s > t || i >= arr.length) return 0;
-
-        int take = solve(s + arr[i], i, t, arr);
-        int skip = solve(s, i + 1, t, arr);
-
-        return take + skip;
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+ 
+        for (int price : prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+ 
+        return profit;
     }
 }
