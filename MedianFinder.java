2cebfac687dc17e7a13808a4bc5c9d9ce47a5a2b
Anuj Kumar <personeluse2000@gmail.com>
2026-04-26 19:40:00 +0530

    changes in code

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,11 +1,14 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        return Math.min (solve(0), solve(1));
-    }
-    solve(int i){
-        if(i==n-1) return 0;
-       int take_1 = arr[i] + solve(i+1);
-       int take_2 = arr[i] + solve(i+2);
-       retrun Math.min(sum , Math.min(take_1 + take_2)) ; ;
+
+        int prev2 = 0, prev1 = 0;
+
+        for (int i = 0; i < cost.length; i++) {
+            int curr = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return Math.min(prev1, prev2);
     }
 }
