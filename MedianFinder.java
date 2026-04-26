98da1e79845ee5f0bbe47251d9fec50c4b66b7c4
Anuj Kumar <personeluse2000@gmail.com>
2026-04-26 19:39:49 +0530

    code 746

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,15 +1,11 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
-        int sell1 = 0, sell2 = 0;
-
-        for (int price : prices) {
-            buy1 = Math.max(buy1, -price);
-            sell1 = Math.max(sell1, buy1 + price);
-            buy2 = Math.max(buy2, sell1 - price);
-            sell2 = Math.max(sell2, buy2 + price);
-        }
-
-        return sell2;
+    public int minCostClimbingStairs(int[] cost) {
+        return Math.min (solve(0), solve(1));
+    }
+    solve(int i){
+        if(i==n-1) return 0;
+       int take_1 = arr[i] + solve(i+1);
+       int take_2 = arr[i] + solve(i+2);
+       retrun Math.min(sum , Math.min(take_1 + take_2)) ; ;
     }
 }
