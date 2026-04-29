f3e694657b35de1d45015fe86740e6dbedda78cf
Anuj Kumar <personeluse2000@gmail.com>
2026-04-29 21:37:29 +0530

    code 1218

--- MedianFinder.java
+++ MedianFinder.java
@@ -1,25 +1,15 @@
 class Solution {
-    public int countGoodStrings(int low, int high, int zero, int one) {
+    public int longestSubsequence(int[] arr, int diff) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        int max = 1;
 
-        int MOD = 1_000_000_007;
-
-        long[] dp = new long[high + 1];
-        dp[0] = 1;
-  
-        long ans = 0;
-
-        for (int i = 1; i <= high; i++) {
-
-            if (i - zero >= 0)
-                dp[i] = (dp[i] + dp[i - zero]) % MOD;
-
-            if (i - one >= 0)
-                dp[i] = (dp[i] + dp[i - one]) % MOD;
-
-            if (i >= low)
-                ans = (ans + dp[i]) % MOD;
+        for (int x : arr) {
+            int prev = dp.getOrDefault(x - diff, 0);
+            int curr = prev + 1;
+            dp.put(x, curr);
+            max = Math.max(max, curr);
         }
 
-        return (int) ans;
+        return max;
     }
 }
