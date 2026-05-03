6060bc9040f38141df027a3a655d9d2449632c2b
Anuj Kumar <personeluse2000@gmail.com>
2026-05-03 19:39:36 +0530

    change

--- Solution.java
+++ Solution.java
@@ -1,37 +1,15 @@
 class Solution {
-    public int sumOfPrimesInRange(int n) {
-
-        int k = n;
-        int rev = 0;
-
-        // reverse number
-        while(n != 0){
-            int j = n % 10;
-            rev = rev * 10 + j;
-            n = n / 10;
-        }
-
-        int mine = Math.min(k, rev);
-        int maxe = Math.max(k, rev);
-
-        int sum = 0;
-
-        for(int i = mine; i <= maxe; i++){
-            if(isPrime(i)){
-                sum += i;
-            }
-        }
-
-        return sum;
+    public int maxFixedPoints(int[] nums) {
+        
     }
+    solve(int i, int j) //starting dono 0 sh ho rha he.
+    {
+        
+        if(nums[i] == j)
+            return 1+solve(i+1, j+1);
 
-    private boolean isPrime(int x){
-        if(x <= 1) return false;
-
-        for(int i = 2; i * i <= x; i++){
-            if(x % i == 0) return false;
-        }
-
-        return true;
+        delete = solve(i+1, j);
+        n_delete = solve(i+1, j+1);
+        return Math.max(delete, n_delete);
     }
 }
--- sql.sql
+++ sql.sql
@@ -1,13 +1,4 @@
-create table customers(
-	cust_id serial primary key,
-	cust_name varchar(10) Not Null
-);
-
-create table ordres(
-	order_id serial primary key,
-	ord_date date not null,
-	price numeric not null,
-	cust_id integer not null,
-	foreign key (cust_id) refrences 
-	customers (cust_id) 
-);
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
