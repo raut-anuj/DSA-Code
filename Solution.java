8e88e494d32f2431eb15cbc114ebe631b92f0f8e
Anuj Kumar <personeluse2000@gmail.com>
2026-05-04 21:46:45 +0530

    combination sum 2 code

--- Solution.java
+++ Solution.java
@@ -1,24 +1,28 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+
         backtrack(0, candidates, target, new ArrayList<>(), res);
         return res;
     }
 
-    private void backtrack(int i, int[] arr, int target, List<Integer> curr, List<List<Integer>> res) {
+    private void backtrack(int start, int[] arr, int target, List<Integer> curr, List<List<Integer>> res) {
         if (target == 0) {
             res.add(new ArrayList<>(curr));
             return;
         }
 
-        if (i >= arr.length || target < 0) return;
+        for (int i = start; i < arr.length; i++) {
 
-        // take (same index allowed)
-        curr.add(arr[i]);
-        backtrack(i, arr, target - arr[i], curr, res);
-        curr.remove(curr.size() - 1);
+            // skip duplicates
+            if (i > start && arr[i] == arr[i - 1]) continue;
 
-        // skip
-        backtrack(i + 1, arr, target, curr, res);
+            if (arr[i] > target) break;
+
+            curr.add(arr[i]);
+            backtrack(i + 1, arr, target - arr[i], curr, res);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
