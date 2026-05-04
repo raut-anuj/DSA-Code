7a47d0d8fa25e608569126208b1a1d9c2d6d5c4e
Anuj Kumar <personeluse2000@gmail.com>
2026-05-04 21:45:27 +0530

    change

--- sql.sql
+++ sql.sql
@@ -1,4 +1,6 @@
-SELECT p.firstName, p.lastName, a.city, a.state
-FROM Person p
-LEFT JOIN Address a
-ON p.personId = a.personId;
+select (
+	select DISTINCT salary
+	from employee
+	order by salary dec
+	limit 1 offset 2
+) as SecondHighestSalary; 
