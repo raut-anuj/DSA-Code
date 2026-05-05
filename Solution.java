ceac222f82733bfd1e106a6ad54317f21d98f67b
Anuj Kumar <personeluse2000@gmail.com>
2026-05-05 20:10:52 +0530

    correct code of matchsticks problem

--- Solution.java
+++ Solution.java
@@ -1,21 +1,47 @@
+import java.util.*;
+
 class Solution {
-    public boolean makesquare(int[] stick) {
-        
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        // total sum divisible hona chahiye
+        if (sum % k != 0) return false;
+
+        int target = sum / k;
+
+        boolean[] used = new boolean[nums.length];
+
+        return solve(nums, k, 0, 0, target, used);
     }
-    solve(idx, sides, stick){
-        if(idx == n){
-            if(sides 0 == 1 ==2== 3) retrun true;
-            return false;
-        }
-        for(int i=0;i<n;i++){
-            if(i>0 && sides[i] == sides[i-1]) continue;
 
-            if(sides[i]+ stick[idx]> target) continue;
+    private boolean solve(int[] nums, int k, int start, int currSum, int target, boolean[] used) {
 
-            sides[i+= stick(idx)];
-            if(solve())
-                retrun true;
-            sidesi[-]=stick9idx0;
+        // sab buckets ban gaye
+        if (k == 0) return true;
+
+        // ek bucket complete ho gaya
+        if (currSum == target) {
+            return solve(nums, k - 1, 0, 0, target, used);
         }
+
+        for (int i = start; i < nums.length; i++) {
+
+            if (used[i]) continue;
+
+            if (currSum + nums[i] > target) continue;
+
+            used[i] = true;
+
+            if (solve(nums, k, i + 1, currSum + nums[i], target, used)) {
+                return true;
+            }
+
+            used[i] = false; // backtrack
+        }
+
+        return false;
     }
 }
