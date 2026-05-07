22f250ac328204d31b4a1dbbdf173500b56f5937
Anuj Kumar <personeluse2000@gmail.com>
2026-05-07 21:50:24 +0530

    code 991

--- Solution.java
+++ Solution.java
@@ -1,34 +1,18 @@
 class Solution {
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<int[]> list = new ArrayList<>();
-        dfs(root, 0, 0, list);
+    public int brokenCalc(int sv, int tv) {
+        int c = 0;
 
-        Collections.sort(list, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0]; // col
-            if (a[1] != b[1]) return a[1] - b[1]; // row
-            return a[2] - b[2]; // value
-        });
+        while (tv > sv) {
 
-        List<List<Integer>> res = new ArrayList<>();
-        int prevCol = Integer.MIN_VALUE;
-
-        for (int[] arr : list) {
-            if (arr[0] != prevCol) {
-                res.add(new ArrayList<>());
-                prevCol = arr[0];
+            if (tv % 2 == 0) {
+                tv = tv / 2;
+            } else {
+                tv = tv + 1;
             }
-            res.get(res.size() - 1).add(arr[2]);
+
+            c++;
         }
 
-        return res;
-    }
-
-    public void dfs(TreeNode node, int row, int col, List<int[]> list) {
-        if (node == null) return;
-
-        list.add(new int[]{col, row, node.val});
-
-        dfs(node.left, row + 1, col - 1, list);
-        dfs(node.right, row + 1, col + 1, list);
+        return c + (sv - tv);
     }
 }
