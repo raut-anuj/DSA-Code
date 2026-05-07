8b1fe76432d87198d3792cbff736a03692c86bce
Anuj Kumar <personeluse2000@gmail.com>
2026-05-07 21:55:31 +0530

    code having some changes

--- Important/Sliding Window Maximum.java
+++ Important/Sliding Window Maximum.java
@@ -1,35 +1,31 @@
-//This is generally known as "Monotonic increasing/decreasing  Queue/Dequeue"
-//Approach-1 (Using Deque) Every element is added(pushed) and popped only once,So it is O(n) time complexity.
-//SEE MY COMMENT IN THE VIDEO where I have explained why we can't take QUEUE to solve this.
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayDeque<Integer> q = new ArrayDeque<>(); 
-        int i=0, j=0, ptr=0; 
-        int n = nums.length; 
-        int[] res = new int[n-k+1]; 
+    public int brokenCalc(int sv, int tv) {
+        int c = 0;
 
-        while(j<n){
-            while(!q.isEmpty() && q.peekLast()<nums[j]){
-                q.pollLast(); 
+        while (tv > sv) {
+
+            if (tv % 2 == 0) {
+                tv = tv / 2;
+            } else {
+                tv = tv + 1;
             }
-        
-            q.add(nums[j]);
 
-            if(j-i+1<k){
-                j++; 
-            }else if(j-i+1==k){
-
-                res[ptr++] = q.peek();
-                
-                if(nums[i]==q.peek()){
-                    q.pollFirst(); 
-                }
-                i++; 
-                j++;
-            }
+            c++;
         }
 
-        return res; 
-
+        return c + (sv - tv);
     }
-}
+} sum =0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum-=nums[i];
+            ans = Math.min(ans,Math.abs(sum-(n-1)*nums[i]));
+            n--;
+        }
+        return ans;
+    }
+}   
