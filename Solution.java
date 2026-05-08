bec8ce0db62fd8901ec4658da06df268b84704f1
Anuj Kumar <personeluse2000@gmail.com>
2026-05-08 22:55:23 +0530

    question number 401

--- Solution.java
+++ Solution.java
@@ -1,18 +1,30 @@
 class Solution {
-    public int brokenCalc(int sv, int tv) {
-        int c = 0;
 
-        while (tv > sv) {
+    List<String> res = new ArrayList<>();
 
-            if (tv % 2 == 0) {
-                tv = tv / 2;
-            } else {
-                tv = tv + 1;
-            }
+    public List<String> readBinaryWatch(int turnedOn) {
 
-            c++;
+        solve(0, 0, 0, turnedOn);
+
+        return res;
+    }
+
+    private void solve(int idx, int hrs, int mins, int left) {
+
+        if (hrs > 11 || mins > 59) return;
+
+        if (left == 0) {
+            res.add(hrs + ":" + (mins < 10 ? "0" + mins : mins));
+            return;
         }
 
-        return c + (sv - tv);
+        for (int i = idx; i < 10; i++) {
+
+            if (i < 4) {
+                solve(i + 1, hrs + (1 << i), mins, left - 1);
+            } else {
+                solve(i + 1, hrs, mins + (1 << (i - 4)), left - 1);
+            }
+        }
     }
 }
