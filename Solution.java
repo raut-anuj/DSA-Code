d67385f0b2ae9e5443fc22f2387dfa1a51463c5c
Anuj Kumar <personeluse2000@gmail.com>
2026-05-08 22:57:45 +0530

    question 254 leetcode

--- Solution.java
+++ Solution.java
@@ -1,15 +1,34 @@
 class Solution {
 
-    public List<Integer> grayCode(int n) {
+    public List<List<Integer>> getFactors(int n) {
 
-        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
 
-        int size = 1 << n;
-
-        for (int i = 0; i < size; i++) {
-            res.add(i ^ (i >> 1));
-        }
+        solve(2, n, new ArrayList<>(), res);
 
         return res;
+    }
+
+    private void solve(int start, int n, List<Integer> curr,
+                       List<List<Integer>> res) {
+
+        if (n == 1) {
+            if (curr.size() > 1) {
+                res.add(new ArrayList<>(curr));
+            }
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+
+            if (n % i == 0) {
+
+                curr.add(i);
+
+                solve(i, n / i, curr, res);
+
+                curr.remove(curr.size() - 1);
+            }
+        }
     }
 }
