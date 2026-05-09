a5abe7ecf6bc55d13a234355de33953f80b64f61
Anuj Kumar <personeluse2000@gmail.com>
2026-05-09 19:51:10 +0530

    changes in code

--- Solution.java
+++ Solution.java
@@ -1,34 +1,14 @@
-class Solution {
+select (
+	select DISTINCT salary
+	from employee
+	order by salary desc
+	limit 1 offset 5
+) as SecondHighestSalary;
 
-    public List<List<Integer>> getFactors(int n) {
-
-        List<List<Integer>> res = new ArrayList<>();
-
-        solve(2, n, new ArrayList<>(), res);
-
-        return res;
-    }
-
-    private void solve(int start, int n, List<Integer> curr,
-                       List<List<Integer>> res) {
-
-        if (n == 1) {
-            if (curr.size() > 1) {
-                res.add(new ArrayList<>(curr));
-            }
-            return;
-        }
-
-        for (int i = start; i <= n; i++) {
-
-            if (n % i == 0) {
-
-                curr.add(i);
-
-                solve(i, n / i, curr, res);
-
-                curr.remove(curr.size() - 1);
-            }
-        }
-    }
-}
+dispite haveling th e same name of column we can use alias to give it a different name and use it in the main query.
+select (
+    select DISTINCT salary
+    from employee
+    order by salary desc
+    limit 1 offset 5
+) as SecondHighestSalary;
