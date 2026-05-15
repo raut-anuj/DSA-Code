954c901264c2818b1307300865fc3942595ade46
Anuj Kumar <personeluse2000@gmail.com>
2026-05-15 19:52:51 +0530

    refactor: replace LRUCache implementation with minFallingPathSum method for minimum falling path sum calculation

--- Solution.java
+++ Solution.java
@@ -1,77 +1,46 @@
-class LRUCache {
+class Solution {
+    public int minFallingPathSum(int[][] g) {
 
-    class Node {
-        int key, val;
-        Node prev, next;
+        int m1v=-1, m1c=-1, m2v=-1, m2c=-1;
+        int n = g.length;
 
-        Node(int k, int v) {
-            key = k;
-            val = v;
-        }
-    }
-
-    Map<Integer, Node> map;
-
-    Node head, tail;
-
-    int cap;
-
-    public LRUCache(int capacity) {
-
-        cap = capacity;
-
-        map = new HashMap<>();
-
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-
-        head.next = tail;
-        tail.prev = head;
-    }
-
-    public int get(int key) {
-
-        if (!map.containsKey(key)) return -1;
-
-        Node node = map.get(key);
-
-        remove(node);
-        insert(node);
-
-        return node.val;
-    }
-
-    public void put(int key, int value) {
-
-        if (map.containsKey(key)) {
-            remove(map.get(key));
+        for(int i=0; i<n; i++){
+            if(m1c==-1 || m1v > g[n-1][i]){
+                m2v=m1v; m2c=m1c;
+                m1v=g[n-1][i]; m1c=i;
+            }
+            else if(m2v==-1 || g[n-1][i] < m2v){
+                m2v=g[n-1][i];
+                m2c=i;
+            }
         }
 
-        if (map.size() == cap) {
-            remove(tail.prev);
+        for(int i=n-2; i>=0; i--){
+
+            int nm1v=-1,nm1c=-1,nm2v=-1,nm2c=-1;
+
+            for(int j=0; j<n; j++){
+
+                int ans;
+
+                if(j!=m1c)
+                    ans = g[i][j] + m1v;
+                else
+                    ans = g[i][j] + m2v;
+
+                if(nm1c==-1 || ans < nm1v){
+                    nm2v=nm1v; nm2c=nm1c;
+                    nm1v=ans; nm1c=j;
+                }
+                else if(nm2c==-1 || ans < nm2v){
+                    nm2v=ans; nm2c=j;
+                }
+            }
+
+            m1v=nm1v; m1c=nm1c;
+            m2v=nm2v; m2c=nm2c;
         }
 
-        insert(new Node(key, value));
-    }
-
-    private void remove(Node node) {
-
-        map.remove(node.key);a
-
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    private void insert(Node node) {
-
-        map.put(node.key, node);
-
-        Node next = head.next;
-
-        head.next = node;
-        node.prev = head;
-
-        node.next = next;
-        next.prev = node;
+        return m1v;
     }
 }
