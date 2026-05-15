ea1daab30cd97b1a8319697a38bd4c327afcb662
Anuj Kumar <personeluse2000@gmail.com>
2026-05-15 19:54:35 +0530

    refactor: replace minFallingPathSum method with maximumBags for bag counting logic

--- Solution.java
+++ Solution.java
@@ -1,46 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
-    public int minFallingPathSum(int[][] g) {
+    public int maximumBags(int[] c, int[] r, int ar) {
 
-        int m1v=-1, m1c=-1, m2v=-1, m2c=-1;
-        int n = g.length;
+        int n = c.length;
+        int[] narr = new int[n];
 
-        for(int i=0; i<n; i++){
-            if(m1c==-1 || m1v > g[n-1][i]){
-                m2v=m1v; m2c=m1c;
-                m1v=g[n-1][i]; m1c=i;
-            }
-            else if(m2v==-1 || g[n-1][i] < m2v){
-                m2v=g[n-1][i];
-                m2c=i;
+        // remaining space
+        for (int i = 0; i < n; i++) {
+            narr[i] = c[i] - r[i];
+        }
+
+        Arrays.sort(narr);
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (narr[i] == 0) {
+                count++;
+            } 
+            else if (narr[i] <= ar) {
+                ar -= narr[i];
+                count++;
+            } 
+            else {
+                break;
             }
         }
 
-        for(int i=n-2; i>=0; i--){
-
-            int nm1v=-1,nm1c=-1,nm2v=-1,nm2c=-1;
-
-            for(int j=0; j<n; j++){
-
-                int ans;
-
-                if(j!=m1c)
-                    ans = g[i][j] + m1v;
-                else
-                    ans = g[i][j] + m2v;
-
-                if(nm1c==-1 || ans < nm1v){
-                    nm2v=nm1v; nm2c=nm1c;
-                    nm1v=ans; nm1c=j;
-                }
-                else if(nm2c==-1 || ans < nm2v){
-                    nm2v=ans; nm2c=j;
-                }
-            }
-
-            m1v=nm1v; m1c=nm1c;
-            m2v=nm2v; m2c=nm2c;
-        }
-
-        return m1v;
+        return count;
     }
 }
