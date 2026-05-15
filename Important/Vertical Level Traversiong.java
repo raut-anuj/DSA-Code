f9f7d96140f00bc9cef82b14499990f160a7e1b0
Anuj Kumar <personeluse2000@gmail.com>
2026-05-15 19:53:36 +0530

    refactor: remove verticalTraversal method and related TreeNode class for maximumBags implementation

--- Important/Vertical Level Traversiong.java
+++ Important/Vertical Level Traversiong.java
@@ -1,60 +1,34 @@
-import java.util.*;
-//   Definition for a binary tree node.
-   class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+import java.util.Arrays;
 
 class Solution {
-    TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> map = new TreeMap<>();
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(root==null){
-            return res;
-        }
-        //col -> [level -> [list]]
-        //dfs - bfs
-        dfs(root,0,0); // dfs call to fill the map
-        // take care of sorting and fill the res;
-        // [column wise sorting]
-        for(Map.Entry<Integer,TreeMap<Integer,ArrayList<Integer>>> entry : map.entrySet()){
-            TreeMap<Integer,ArrayList<Integer>> levelMap = entry.getValue();
-            ArrayList<Integer> list = new ArrayList<>();
-            
-            for(Map.Entry<Integer,ArrayList<Integer>> subEntry : levelMap.entrySet()){
-                ArrayList<Integer> sublist = subEntry.getValue();           
-                Collections.sort(sublist); 
-                list.addAll(sublist);
-            }
-            res.add(list);
-            // [[-2],[-1],...]
-        }
-        return res;
+    public int maximumBags(int[] c, int[] r, int ar) {
 
-    }
-    public void dfs(TreeNode root, int col, int level){
-        if(root==null){
-            return;
+        int n = c.length;
+        int[] narr = new int[n];
+
+        // remaining space
+        for (int i = 0; i < n; i++) {
+            narr[i] = c[i] - r[i];
         }
-        // insert in map
-        // col
-        if(!map.containsKey(col)){
-            map.put(col,new TreeMap<>());
+
+        Arrays.sort(narr);
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (narr[i] == 0) {
+                count++;
+            } 
+            else if (narr[i] <= ar) {
+                ar -= narr[i];
+                count++;
+            } 
+            else {
+                break;
+            }
         }
-        //level
-        if(!map.get(col).containsKey(level)){
-            map.get(col).put(level,new ArrayList<>());
-        }
-        map.get(col).get(level).add(root.val);
-        dfs(root.left,col-1,level+1);
-        dfs(root.right,col+1,level+1);
+
+        return count;
     }
 }
